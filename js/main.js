bc0b61be4c549be5035075ddafacfbc847b43c02
soraja721547 <soraja721547@gmail.com>
2019-05-13 18:04:40 +0800

    search進度80％

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-// $('.btn').click(function(){
-//     $('.btn').css('background','black');
-// })
+$('.fa-angle-double-left').click(function(){
+    $('nav').css('width','0');
+})
+
+
+    $('.nav-link').click(function(){
+        if($('.nav-link') != $(this)){
+            $('.nav-link').removeClass('active');
+            $(this).toggleClass('active');
+        };
+    })
+
