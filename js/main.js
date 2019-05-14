903b3804d30c3423b8418ff3ee8ceeb29014b564
soraja721547 <soraja721547@gmail.com>
2019-05-14 18:01:30 +0800

    tab测试

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 $('.fa-angle-double-left').click(function(){
-    $('nav').css('width','0');
+    $('nav').toggleClass('navmini');
+    $('.fa-angle-double-left').toggleClass('icon-rotate');
+    $('.sidebar').toggleClass('d-none');
+    $('.main-content').toggleClass('main-content-full');
 })
 
+$('.index-tab').click(function(){
+    $('.index-tab').removeClass('active');
+    $(this).addClass('active');
+})
 
     $('.nav-link').click(function(){
-        if($('.nav-link') != $(this)){
+        // if($('.nav-link') != $(this)){
             $('.nav-link').removeClass('active');
-            $(this).toggleClass('active');
-        };
+            $(this).addClass('active');
+        // };
     })
 
